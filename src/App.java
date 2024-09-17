44591682aa766ea2d5165a18c9141898aebd6f6f
Adry <adrydlg752@gmailcom>
2024-09-17 18:07:09 +0200

    Add_Operations_class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import Operations.*;
 public class App {
     public static void main(String[] args) throws Exception {
+        calculate cal = new calculate();
+
+        int res = cal.pot(2, 3);
         System.out.println("Hello, World!");
     }
 }
---
+++ src/Operations/calculate.java
@@ -0,0 +1,25 @@
+package Operations;
+
+public class calculate {
+  public int sum(int num1, int num2) {
+    return num1 + num2;
+  }
+  public int rest(int num1, int num2) {
+    return num1 + num2;
+  }
+
+  public int mult(int num1, int num2) {
+    return num1 + num2;
+  }
+  public int div(int num1, int num2) {
+    return num1 + num2;
+  }
+
+  public int pot(int num1, int exp) {
+    
+    for (int i = 0 ;i < exp ;i++){
+      num1 *= num1;
+    }
+    return num1;
+  }
+}
